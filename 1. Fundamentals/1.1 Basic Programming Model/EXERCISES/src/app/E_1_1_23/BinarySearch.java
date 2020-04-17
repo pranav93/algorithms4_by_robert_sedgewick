7866fa0dd328134014bf2759e4d62e62c043fe09
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-04-17 18:01:14 +0530

    done 1.1.23

---
+++ 1. Fundamentals/1.1 Basic Programming Model/EXERCISES/src/app/E_1_1_23/BinarySearch.java
@@ -0,0 +1,40 @@
+package app.E_1_1_23;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.*;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a) { // Array must be sorted.
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) { // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whitelist = In.readInts(args[0]);
+        String s = args[1];
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty()) { // Read key, print if not in whitelist.
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) < 0) {
+                if (s.equals("+")) {
+                    StdOut.println(key);                    
+                }
+            } else {
+                if (s.equals("-")) {
+                    StdOut.println(key);                    
+                }
+            }
+        }
+    }
+}
