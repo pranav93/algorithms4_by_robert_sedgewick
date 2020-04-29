be7047877996138f73690b5b197fd6f3a6ff23ed
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-04-29 09:02:58 -0700

    done exercise 1.3.27

---
+++ 1. Fundamentals/1.3 Bags, Queues, and Stacks/Exercises/src/E_1_3_27/LinkedList.java
@@ -0,0 +1,137 @@
+package E_1_3_27;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class LinkedList {
+    class Node {
+        int item;
+        Node next;
+    }
+
+    Node first;
+    int N;
+
+    public void insertFirst(int item) {
+        Node temp = new Node();
+        temp.item = item;
+        temp.next = this.first;
+        this.first = temp;
+        this.N++;
+    }
+
+    public int removeLast() {
+        Node current = this.first;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        Node prev = current;
+        current = current.next;
+        int item = current.item;
+        prev.next = null;
+        this.N--;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return this.N == 0;
+    }
+
+    public int size() {
+        return this.N;
+    }
+
+    public void listContent() {
+        Node current = this.first;
+        StdOut.printf("First -> \t");
+        while (current != null) {
+            StdOut.printf("Node [ %s ] -> \t", current.item);
+            current = current.next;
+        }
+        StdOut.printf("NULL\n");
+    }
+
+    public int delete(int k) {
+        Node current = this.first;
+        while (k != 1) {
+            current = current.next;
+            k--;
+        }
+        Node prev = current;
+        current = current.next;
+        int item = current.item;
+        prev.next = current.next;
+        this.N--;
+        return item;
+    }
+
+    public boolean contains(int key) {
+        Node current = this.first;
+        while (current != null) {
+            if (current.item == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean removeAfter(int key) {
+        Node current = this.first;
+        while (current != null && current.item != key) {
+            current = current.next;
+        }
+        if (current == null || current.next == null) {
+            return false;
+        }
+        Node target = current.next;
+        current.next = target.next;
+        this.N--;
+        return true;
+    }
+
+    public boolean insertAfter(int key, int val) {
+        Node current = this.first;
+        while (current != null && current.item != key) {
+            current = current.next;
+        }
+        if (current == null) {
+            return false;
+        }
+        Node temp = new Node();
+        temp.item = val;
+        temp.next = current.next;
+        current.next = temp;
+        this.N++;
+        return true;
+    }
+
+    public void remove(int key) {
+        Node current = this.first;
+        Node prev = null;
+        while (current != null) {
+            if (current.item == key) {
+                if (prev == null) {
+                    this.first = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                this.N--;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+    }
+
+    public int max() {
+        int maxVal = 0;
+        Node current = this.first;
+        while (current != null) {
+            if (current.item > maxVal) {
+                maxVal = current.item;
+            }
+            current = current.next;
+        }
+        return maxVal;
+    }
+}
