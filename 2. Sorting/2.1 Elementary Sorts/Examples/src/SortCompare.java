06913e7b00e27689d39013c8f92d33963f7147ce
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-05-12 15:24:01 +0530

    2.1 examples wip

---
+++ 2. Sorting/2.1 Elementary Sorts/Examples/src/SortCompare.java
@@ -0,0 +1,41 @@
+import InsertionSort.InsertionSort;
+import SelectionSort.SelectionSort;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class SortCompare {
+    public static double time(String alg, Double[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Insertion")) {
+            InsertionSort.sort(a);
+        }
+        if (alg.equals("Selection")) {
+            SelectionSort.sort(a);
+        }
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+        Double[] a = new Double[N];
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < N; j++) {
+                a[j] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+        double t1 = timeRandomInput(alg1, N, T);
+        double t2 = timeRandomInput(alg2, N, T);
+        StdOut.printf("For %d random Doubles\n %s is ", N, alg1);
+        StdOut.printf("%.1f times faster than %s\n", t1 / t2, alg2);
+    }
+}
