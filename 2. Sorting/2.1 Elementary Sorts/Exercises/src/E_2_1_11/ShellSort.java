b075e522ad4fb386e05dd934935dbb27db08c211
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-05-15 11:06:33 +0530

    Done exercise 2.1.11

---
+++ 2. Sorting/2.1 Elementary Sorts/Exercises/src/E_2_1_11/ShellSort.java
@@ -0,0 +1,58 @@
+package E_2_1_11;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ShellSort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h <= N) {
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
+            }
+            h /= 3;
+        }
+    }
+
+    private static void exch(Comparable[] a, int j, int i) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void show(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
