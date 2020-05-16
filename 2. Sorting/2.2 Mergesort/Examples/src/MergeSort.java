4fbfea923117d4a04a37867190a4c35039307e0f
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-05-16 08:14:19 +0530

    WIP examples 2.2

---
+++ 2. Sorting/2.2 Mergesort/Examples/src/MergeSort.java
@@ -0,0 +1,71 @@
+import edu.princeton.cs.algs4.Merge;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class MergeSort {
+    Comparable[] aux;
+
+    MergeSort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private void sort(Comparable[] a, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    private void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo;
+        int j = mid + 1;
+
+        for (int k = lo; k <= hi; k++) {
+            this.aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = this.aux[j++];
+            } else if (j > hi) {
+                a[k] = this.aux[i++];
+            } else if (less(this.aux[i], this.aux[j])) {
+                a[k] = this.aux[i++];
+            } else {
+                a[k] = this.aux[j++];
+            }
+        }
+    }
+
+    private boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readStrings();
+        MergeSort m = new MergeSort(a);
+        assert m.isSorted(a);
+        m.show(a);
+    }
+}
