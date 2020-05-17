c0aa52e8e9d72898697b221e77b9b794613b2e36
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-05-17 18:34:57 +0530

    Done exercises 2.2.6

--- 2. Sorting/2.2 Mergesort/Exercises/src/E_2_2_6/MergeSortBU.java
+++ 2. Sorting/2.2 Mergesort/Exercises/src/E_2_2_6/MergeSortBU.java
@@ -1,45 +1,59 @@
 package E_2_2_6;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class MergeSortBU {
     Comparable[] aux;
+    int accesses;
 
     MergeSortBU(Comparable[] a) {
         this.aux = new Comparable[a.length];
-        this.sort(a);
+        int accesses = 0;
+        accesses = this.sort(a, accesses);
+        this.accesses = accesses;
     }
 
-    private void sort(Comparable[] a) {
+    private int sort(Comparable[] a, int accesses) {
         int N = a.length;
 
         for (int sz = 2; sz < N; sz *= 2) {
             for (int lo = 0; lo < N; lo += (sz * 2)) {
-                this.merge(a, lo, lo + sz - 1, lo + (sz * 2));
+                accesses += this.merge(a, lo, lo + sz - 1, lo + (sz * 2), accesses);
             }
         }
+
+        return accesses;
     }
 
-    private void merge(Comparable[] a, int lo, int mid, int hi) {
+    private int merge(Comparable[] a, int lo, int mid, int hi, int accesses) {
         int i = lo;
         int j = hi;
 
         for (int k = lo; k <= hi; i++) {
             this.aux[k] = a[k];
+            accesses += 2;
         }
 
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 a[k] = this.aux[j++];
+                accesses += 2;
             } else if (j > hi) {
                 a[k] = this.aux[i++];
+                accesses += 2;
             } else if (this.less(this.aux[j], this.aux[i])) {
                 a[k] = this.aux[j++];
+                accesses += 4;
             } else {
                 a[k] = this.aux[i++];
+                accesses += 4;
             }
         }
+
+        return accesses;
     }
 
     private boolean less(Comparable v, Comparable w) {
@@ -65,9 +79,29 @@ public class MergeSortBU {
     }
 
     public static void main(String[] args) {
-        String[] s = StdIn.readStrings();
-        MergeSortTD m = new MergeSortTD(s);
-        assert m.isSorted(s);
-        m.show(s);
+        StdDraw.setCanvasSize(1000, 1000);
+        StdDraw.setXscale(-1, 513);
+        StdDraw.setYscale(-1, 28000);
+        StdDraw.line(0, -1, 0, 28000);
+        StdDraw.line(-1, 0, 512, 0);
+
+        StdDraw.setPenRadius(0.01);
+
+        for (int sz = 1; sz <= 512; sz++) {
+            Double[] s = new Double[sz];
+            for (int i = 0; i < sz; i++) {
+                s[i] = StdRandom.uniform();
+            }
+            MergeSortTD m = new MergeSortTD(s);
+            assert m.isSorted(s);
+            // m.show(s);
+            double formulae = 6 * sz * Math.log(sz) / Math.log(2);
+            StdOut.printf("Size -> %d | Accesses -> %d | 6 * N * lgN -> %f \n", sz, m.accesses, formulae);
+
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.point(sz, formulae);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.point(sz, m.accesses);
+        }
     }
 }
