147a0e701186a2f6c696d0d0042c0935868bf366
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-05-18 18:03:13 +0530

    WIP examples 2.3

---
+++ 2. Sorting/2.3 Quicksort/Examples/src/Quick/Quick.java
@@ -0,0 +1,79 @@
+package Quick;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick {
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+
+        if (hi <= lo)
+            return;
+
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+
+        while (true) {
+            while (less(a[++i], v))
+                if (i == hi)
+                    break;
+            while (less(v, a[--j]))
+                if (j == lo)
+                    break;
+            if (j <= i)
+                break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void show(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+        String[] s = StdIn.readStrings();
+        Quick q = new Quick();
+        q.sort(s);
+        assert q.isSorted(s);
+        q.show(s);
+    }
+}
