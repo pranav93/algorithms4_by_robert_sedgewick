482e7644c795e1e171d15aaf043de3686b9aeb0f
Pranav <ambhore.pranav@gmail.com>
2020-06-23 06:02:20 -0700

    exercise 3.2.14 WIP

---
+++ 3. Searching/3.2 Binary Search Trees/Exercises/src/E_3_2_14/Client.java
@@ -0,0 +1,31 @@
+package E_3_2_14;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Client {
+    public static void main(String[] args) {
+        String[] strInput = "S E R C H E X M P L E".split(" ");
+        BST<String, Integer> st = new BST<String, Integer>();
+
+        for (int i = 0; i < strInput.length; i++) {
+            StdOut.println("Putting " + strInput[i] + " -> " + i);
+            st.put(strInput[i], i);
+        }
+
+        for (int i = 0; i < strInput.length; i++) {
+            StdOut.println("get(" + strInput[i] + ") -> " + st.get(strInput[i]));
+        }
+
+        StdOut.println("min() -> " + st.min());
+        StdOut.println("max() -> " + st.max());
+
+        String[] strArr = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
+        for (int i = 0; i < strArr.length; i++) {
+            StdOut.println("floor(" + strArr[i] + ") -> " + st.floor(strArr[i]));
+        }
+        for (int i = 0; i < strArr.length; i++) {
+            StdOut.println("ceiling(" + strArr[i] + ") -> " + st.ceiling(strArr[i]));
+        }
+    }
+
+}
