6de68eb6dc9109e9f0dafd5652c7e367da57bee4
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-07-25 20:41:46 +0530

    done degrees of separation

---
+++ 4. Graphs/4.1 Undirected Graphs/Examples/src/DegreesOfSeparation/DegreesOfSeparation.java
@@ -0,0 +1,38 @@
+package DegreesOfSeparation;
+
+import SymbolGraph.SymbolGraph;
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class DegreesOfSeparation {
+    public static void main(String[] args) {
+        SymbolGraph sg = new SymbolGraph(args[0], args[1]);
+        Graph G = sg.G();
+
+        String source = args[2];
+        if (!sg.contains(source)) {
+            StdOut.println(source + " is not in database.");
+            return;
+        }
+        int s = sg.index(source);
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
+
+        while (StdIn.hasNextLine()) {
+            String sink = StdIn.readLine();
+            if (!sg.contains(sink)) {
+                StdOut.println(sink + " is not in database.");
+                continue;
+            }
+            int t = sg.index(sink);
+            if (bfs.hasPathTo(t)) {
+                for (int p : bfs.pathTo(t)) {
+                    StdOut.println("\t" + sg.name(p));
+                }
+            } else {
+                StdOut.println(sink + " is not connected to " + source);
+            }
+        }
+    }
+}
