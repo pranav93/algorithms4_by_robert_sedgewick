c0e6411a92d3f5c0fda8d81e6830647d057f86b2
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-08-01 14:40:07 +0530

    wip E_4_1_26

---
+++ 4. Graphs/4.1 Undirected Graphs/Examples/src/CycleLoop/Client.java
@@ -0,0 +1,17 @@
+package CycleLoop;
+
+import Graph.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Client {
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        Cycle c = new Cycle(G);
+
+        StdOut.println("hasCycle -> " + c.hasCycle());
+        StdOut.println("parallelEdge -> " + c.parallelEdge());
+        StdOut.println("selfLoop -> " + c.selfLoop());
+    }
+}
---
+++ 4. Graphs/4.1 Undirected Graphs/Examples/src/CycleLoop/Cycle.java
@@ -0,0 +1,69 @@
+package CycleLoop;
+
+import Graph.Graph;
+import edu.princeton.cs.algs4.SET;
+
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+    private boolean parallelEdge;
+    private boolean selfLoop;
+
+    Cycle(Graph G) {
+        this.marked = new boolean[G.V()];
+        this.findLoopParallelEdge(G);
+        if (this.hasCycle) {
+            return;
+        }
+        for (int v = 0; v < G.V(); v++) {
+            if (!this.marked[v]) {
+                this.dfs(G, v);
+            }
+        }
+    }
+
+    private void findLoopParallelEdge(Graph G) {
+        for (int i = 0; i < G.V(); i++) {
+            SET<Integer> adjSet = new SET<Integer>();
+            for (int w : G.adj(i)) {
+                if (w == i) {
+                    // self loop found
+                    this.hasCycle = true;
+                    this.selfLoop = true;
+                    return;
+                }
+                if (!adjSet.contains(w)) {
+                    adjSet.add(w);
+                } else {
+                    // parallel edge found
+                    this.parallelEdge = true;
+                    this.hasCycle = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void dfs(Graph G, int v) {
+        this.marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!this.marked[w]) {
+                this.dfs(G, w);
+            } else if (w != v) {
+                this.hasCycle = true;
+            }
+        }
+    }
+
+    public boolean hasCycle() {
+        return this.hasCycle;
+    }
+
+    public boolean parallelEdge() {
+        return this.parallelEdge;
+    }
+
+    public boolean selfLoop() {
+        return this.selfLoop;
+    }
+}
