e84b8134a17aec88b4fb0efa882a4c0ce01f06ff
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-08-04 14:36:34 +0530

    added diagraph

---
+++ 4. Graphs/4.2 Directed Graphs/Examples/src/Digraph/Client.java
@@ -0,0 +1,11 @@
+package Digraph;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Client {
+    public static void main(String[] args) {
+        Digraph dg = new Digraph(new In(args[0]));
+        StdOut.println(dg);
+    }
+}
---
+++ 4. Graphs/4.2 Directed Graphs/Examples/src/Digraph/Digraph.java
@@ -0,0 +1,69 @@
+package Digraph;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    Digraph(int v) {
+        this.V = v;
+        this.E = 0;
+        this.adj = (Bag<Integer>[]) new Bag[this.V];
+        for (int i = 0; i < this.V; i++) {
+            this.adj[i] = new Bag<Integer>();
+        }
+    }
+
+    Digraph(In in) {
+        this(in.readInt());
+        int e = in.readInt();
+        for (int i = 0; i < e; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            this.addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return this.V;
+    }
+
+    public int E() {
+        return this.E;
+    }
+
+    public void addEdge(int v, int w) {
+        this.adj[v].add(w);
+        this.E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return this.adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(this.V);
+        for (int i = 0; i < this.V; i++) {
+            for (int w : this.adj(i)) {
+                R.addEdge(w, i);
+            }
+        }
+        return R;
+    }
+
+    @Override
+    public String toString() {
+        String out = "Directed Graph with vertices " + this.V + " and edges " + this.E + "\n";
+        for (int i = 0; i < this.V; i++) {
+            out += i + " ->";
+            for (int w : this.adj(i)) {
+                out += " " + w;
+            }
+            out += "\n";
+        }
+        return out;
+    }
+}
